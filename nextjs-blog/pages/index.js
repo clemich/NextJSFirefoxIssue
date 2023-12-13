87e22a695364fa2fd9961f68ce2107b60395ee64
Clement <clement.hallet@gmail.com>
2023-12-13 11:23:43 +0100

    first commit

---
+++ nextjs-blog/pages/index.js
@@ -0,0 +1,121 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+const fruitElements = [];
+
+const fruitArray = ["banana","apple","orange"];
+for (const property in fruitArray) {
+  fruitElements.push(
+    <div key={property} className={styles.card}>
+        <h3>{`Array element ${property}`}</h3>
+        <p>{`Property: ${fruitArray[property]}`}</p>
+      </div>
+  );
+}
+
+// Get the user agent string
+const userAgent = navigator.userAgent;
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+
+      <h3>From a table of ["banana","apple","orange"], we have another one player when iterating the table with a for...in in older version of FireFox (for instance FF 78.11)</h3>
+      <p>Current user agent: ${userAgent}</p>
+
+        <div className={styles.grid}>
+          {/* Affichage des éléments générés par la boucle for...of */}
+          {fruitElements}
+        </div>
+
+      </main>
+
+      <footer>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
+        </a>
+      </footer>
+
+      <style jsx>{`
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+        footer img {
+          margin-left: 0.5rem;
+        }
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          text-decoration: none;
+          color: inherit;
+        }
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family:
+            Menlo,
+            Monaco,
+            Lucida Console,
+            Liberation Mono,
+            DejaVu Sans Mono,
+            Bitstream Vera Sans Mono,
+            Courier New,
+            monospace;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family:
+            -apple-system,
+            BlinkMacSystemFont,
+            Segoe UI,
+            Roboto,
+            Oxygen,
+            Ubuntu,
+            Cantarell,
+            Fira Sans,
+            Droid Sans,
+            Helvetica Neue,
+            sans-serif;
+        }
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  );
+}
